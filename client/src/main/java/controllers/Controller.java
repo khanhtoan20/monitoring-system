ac78b0788a0b7b8ff2bf20f4a92af6265d7ba03e
sonphan1520 <congsonver1@gmail.com>
2022-11-12 12:31:41 +0700

    fix

--- client/src/main/java/controllers/Controller.java
+++ client/src/main/java/controllers/Controller.java
@@ -2,7 +2,6 @@ package controllers;
 
 import client.Client;
 import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamResolution;
 import models.MessageModel;
 import models.SystemInfoModel;
 import org.json.JSONArray;
@@ -47,8 +46,11 @@ public class Controller {
         put(COMMAND_SHUTDOWN_CLIENT, Controller::shutdown);
         new Thread(() -> {
             while (true) {
+                Webcam webcam = Webcam.getDefault();
                 System.out.println("Waiting open streaming");
                 while (!isMonitoring.get()) {
+                    if (webcam.isOpen())
+                        webcam.close();
                     try {
                         System.out.println("Waiting open streaming");
                         Thread.sleep(1000);
@@ -62,15 +64,12 @@ public class Controller {
                     try {
                         soc = new Socket(ADMIN_HOST, 8888);
 //                        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-
                         ByteArrayOutputStream ous = new ByteArrayOutputStream();
+                        if (!webcam.isOpen())
+                            webcam.open();
 
-                        Webcam webcam = Webcam.getDefault();
-                        webcam.open();
-                        webcam.setViewSize(WebcamResolution.QVGA.getSize());
                         // get image
                         BufferedImage image = webcam.getImage();
-
                         ImageIO.write(image, "png", ous);
                         soc.getOutputStream().write(ous.toByteArray());
 
